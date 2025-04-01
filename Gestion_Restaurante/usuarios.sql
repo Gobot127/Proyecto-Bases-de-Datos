9c5d08b809deda14c1de7302ea2e54dd738045d3
Marcopro12345 <morachaconmarco@gmail.com>
2025-04-01 12:53:56 -0600

    creacion de usuarios

--- Gestion_Restaurante/usuarios.sql
+++ Gestion_Restaurante/usuarios.sql
@@ -1,1 +1,13 @@
+/*select usuario*/
 
+CREATE ROLE lector WITH LOGIN PASSWORD 'usuarioSelect123';
+GRANT CONNECT ON DATABASE gestion_restaurante TO lector;
+GRANT USAGE ON SCHEMA public TO lector;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO lector;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO lector;
+
+
+/*super usuario*/
+CREATE ROLE admin_restaurante WITH LOGIN PASSWORD 'usuarioSuper123' SUPERUSER;
+
+SELECT rolname, rolsuper, rolcreaterole, rolcreatedb FROM pg_roles;
