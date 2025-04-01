ef18bf97bc7534abca8aeb6114c99f4762638fac
Felix-alvx <gabi14alv@gmail.com>
2025-04-01 13:22:06 -0600

    Creacion de la primera vista

--- Gestion_Restaurante/Views.sql
+++ Gestion_Restaurante/Views.sql
@@ -1,1 +1,18 @@
+--Vista de Resumen de pedidos
+CREATE VIEW Vista_Resumen_Pedidos AS
+SELECT 
+    P.ID_pedido,
+    C.nombre AS Cliente,
+    M.numero_mesa AS Mesa,
+    E.nombre AS Empleado,
+    F.total AS Total_Pagado,
+    F.metodo_pago AS Metodo_Pago,
+    P.fecha_pedido
+FROM Pedidos P
+LEFT JOIN Clientes C ON P.ID_cliente = C.ID_cliente
+LEFT JOIN Mesas M ON P.ID_mesa = M.ID_mesa
+LEFT JOIN Empleados E ON P.ID_empleado = E.ID_empleado
+LEFT JOIN Facturacion F ON P.ID_pedido = F.ID_pedido;
 
+--LLama al View para ver los detalles del pedido
+SELECT * FROM Vista_Resumen_Pedidos;
