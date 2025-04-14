481655f96352f2b162d01ad7470a5a6ca451bdbc
Luis Gustavo <lgmoreirah@hotmail.com>
2025-04-14 12:28:57 -0700

    Creacion de la segunda funcion calcular_ventas_por_fecha

--- Gestion_Restaurante/Funciones.sql
+++ Gestion_Restaurante/Funciones.sql
@@ -18,3 +18,27 @@ $$ LANGUAGE plpgsql;
 --Ejemplo de como utilizarla
 INSERT INTO Facturacion(ID_pedido, fecha, total, metodo_pago)
 VALUES (1, CURRENT_TIMESTAMP, calcular_total_pedido(1), 'Sinpe Movil');
+
+
+
+--Funcion para saber ventas por fecha especifica
+
+
+CREATE OR REPLACE FUNCTION calcular_ventas_por_fecha(fecha_parametro DATE)
+RETURNS DECIMAL(10,2) AS $$
+DECLARE
+    total_ventas DECIMAL(10,2) := 0;
+BEGIN
+    SELECT  COALESCE(SUM(total), 0)
+    INTO total_ventas
+    FROM Facturacion
+    WHERE DATE(fecha) = fecha_parametro;
+
+    RETURN total_ventas;
+END;
+$$ LANGUAGE plpgsql;
+
+
+--Ejemplo de Como utilizarla
+
+SELECT calcular_ventas_por_fecha('2025-04-14');
